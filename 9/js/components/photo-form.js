9120a523c3e65b0e680cb53a74e99d07783b94c4
Keks <keks@htmlacademy.ru>
2026-03-25 13:23:55 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/components/photo-form.js
@@ -0,0 +1,171 @@
+import {
+  HASHTAG_MAX_COUNT,
+  HASHTAG_MAX_LENGTH,
+  HASHTAG_REG_EXP,
+  HASHTAG_SEPARATOR,
+  HashtagErrorMessage,
+  HIDE_ELEMENT_CLASS,
+  MODAL_OPEN_CLASS,
+  PhotoFilter
+} from '../constants.js';
+import { getElement } from '../element-cache.js';
+import { capitalize, interceptEscInsideInput, trimAndSplit } from '../utils.js';
+
+let validator = null;
+
+/** @type {[validator: (value: string) => boolean, message: string][]} */
+const hashtagValidators = [
+  [
+    (hashtagsStr) => {
+      const hashtags = trimAndSplit(hashtagsStr, HASHTAG_SEPARATOR);
+      return hashtags.every((hashtag) => (HASHTAG_REG_EXP).test(hashtag));
+    },
+    HashtagErrorMessage.AllowedChars
+  ],
+  [
+    (hashtagsStr) => {
+      const hashtags = trimAndSplit(hashtagsStr, HASHTAG_SEPARATOR);
+      return hashtags.every((hashtag) => hashtag.length <= HASHTAG_MAX_LENGTH);
+    },
+    HashtagErrorMessage.MaxLength
+  ],
+  [
+    (hashtagsStr) => {
+      const hashtags = trimAndSplit(hashtagsStr, HASHTAG_SEPARATOR);
+      return hashtags.length <= HASHTAG_MAX_COUNT;
+    },
+    HashtagErrorMessage.MaxCount
+  ],
+  [
+    (hashtagsStr) => {
+      const normalizedHashtags = trimAndSplit(hashtagsStr, HASHTAG_SEPARATOR)
+        .map((hashtag) => hashtag.toLowerCase());
+      const hashtagCounter = new Set(normalizedHashtags);
+
+      return normalizedHashtags.length === hashtagCounter.size;
+    },
+    HashtagErrorMessage.Duplications
+  ],
+];
+
+/**
+ *
+ * @param {SubmitEvent} evt
+ */
+const handleFormSubmit = (evt) => {
+  evt.preventDefault();
+  if (validator.validate()) {
+    evt.target.submit();
+  }
+};
+
+/**
+ *
+ * @param {ChangeEvent} evt
+ */
+const handleChangeFilter = (evt) => {
+  const target = evt.target;
+
+  if (!(target instanceof HTMLInputElement && target.type === 'radio')) {
+    return;
+  }
+
+  const effectLevelElement = getElement('.img-upload__effect-level .effect-level__value');
+  const photoPreviewElement = getElement('.img-upload__preview img');
+  let styles = '';
+  const filter = PhotoFilter[capitalize(target.value)];
+
+  if (filter) {
+    const mean = (filter.Min + filter.Max) / 2;
+    effectLevelElement.step = filter.Step;
+    effectLevelElement.value = mean;
+    styles = filter.Template(mean);
+  }
+
+  photoPreviewElement.style.filter = styles;
+};
+
+export const openPhotoForm = () => {
+  const uploadFormElement = getElement('#upload-select-image');
+  const formOverlayElement = getElement('.img-upload__overlay');
+  const formFiltersElement = getElement('.img-upload__effects', formOverlayElement);
+  const hashtagsFieldElement = getElement('.text__hashtags', formOverlayElement);
+  const closeFormButton = getElement('.img-upload__cancel', formOverlayElement);
+
+  validator = new Pristine(uploadFormElement, {
+    classTo: 'img-upload__field-wrapper',
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextClass: 'img-upload__field-wrapper--error',
+  });
+
+  hashtagValidators.forEach((hashtagValidator) => {
+    validator.addValidator(hashtagsFieldElement, ...hashtagValidator);
+  });
+
+  formFiltersElement.addEventListener('change', handleChangeFilter);
+  closeFormButton.addEventListener('click', handleCloseClick);
+  uploadFormElement.addEventListener('keydown', interceptEscInsideInput);
+  uploadFormElement.addEventListener('submit', handleFormSubmit);
+  window.addEventListener('keydown', handleEscKeydown);
+
+  formOverlayElement.classList.remove(HIDE_ELEMENT_CLASS);
+  document.body.classList.add(MODAL_OPEN_CLASS);
+};
+
+export const closePhotoForm = () => {
+  const uploadFormElement = getElement('#upload-select-image');
+  const formOverlayElement = getElement('.img-upload__overlay');
+  const closeFormButton = getElement('.img-upload__cancel', formOverlayElement);
+  const formFiltersElement = getElement('.img-upload__effects', formOverlayElement);
+  const effectLevelElement = getElement('.img-upload__effect-level .effect-level__value');
+  const photoPreviewElement = getElement('.img-upload__preview img');
+
+  validator.destroy();
+  validator = null;
+
+  uploadFormElement.reset();
+  effectLevelElement.step = 'any';
+  effectLevelElement.value = '';
+  photoPreviewElement.style.filter = '';
+
+  formFiltersElement.removeEventListener('change', handleChangeFilter);
+  closeFormButton.removeEventListener('click', handleCloseClick);
+  uploadFormElement.removeEventListener('keydown', interceptEscInsideInput);
+  uploadFormElement.removeEventListener('submit', handleFormSubmit);
+  window.removeEventListener('keydown', handleEscKeydown);
+
+  formOverlayElement.classList.add(HIDE_ELEMENT_CLASS);
+  document.body.classList.remove(MODAL_OPEN_CLASS);
+};
+
+/**
+ * Handles close button click event when photo viewer is open
+ *
+ * @param {MouseEvent} evt
+ */
+function handleCloseClick(evt) {
+  evt.preventDefault();
+  closePhotoForm();
+}
+
+/**
+  * Handles escape keydown event when photo viewer is open
+  *
+  * @param {KeyboardEvent} evt
+  */
+function handleEscKeydown(evt) {
+  if (evt.code === 'Escape') {
+    closePhotoForm();
+  }
+}
+
+/**
+ * Handle change event on input[type='file'] element
+ * and opens form for uploading image
+ *
+ * @param {ChangeEvent} evt
+ */
+export const handleUploadImgInput = (evt) => {
+  evt.preventDefault();
+  openPhotoForm();
+};
---
+++ 9/js/functions.js
@@ -0,0 +1,92 @@
+import { parseTime } from './utils.js';
+
+/**
+ * Checks that the string is shorter than or equal to the specified length
+ *
+ * @param {string} str - String to be checked
+ * @param {number} maxLen - Maximum string length
+ * @returns {boolean}
+ */
+export const isNotLonger = (str, maxLen) => {
+  const segmenter = new Intl.Segmenter();
+  const segmenterIter = segmenter.segment(str)[Symbol.iterator]();
+
+  let strLen = 0;
+  while (!segmenterIter.next().done) {
+    strLen += 1;
+  }
+
+  return strLen <= maxLen;
+};
+
+/**
+ * Checks that the string is palindrome
+ *
+ * @param {string} str - String to be checked
+ * @returns {boolean}
+ */
+export const isPalindrome = (str) => {
+  const isPunctuation = /\p{P}|\p{Z}/u;
+  const segmenter = new Intl.Segmenter();
+
+  /** @type {string[]} */
+  const chars = [];
+  for (const { segment } of segmenter.segment(str)) {
+    if (!isPunctuation.test(segment)) {
+      chars.push(segment);
+    }
+  }
+
+  const collator = new Intl.Collator(undefined, {
+    usage: 'search',
+    sensitivity: 'accent',
+  });
+  for (let i = 0, j = chars.length - 1; i < j; ++i, --j) {
+    if (collator.compare(chars[i], chars[j]) !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+/**
+ * Collects a number from the digits found in an argument
+ *
+ * @param {string | number} strOrNum - Arbitrary text or number
+ * @returns {number}
+ */
+export const collectNumber = (strOrNum) => {
+  const isDigit = /\d/;
+  const str = strOrNum.toString();
+
+  /** @type {string[]} */
+  const collectedDigits = [];
+  for (const char of str) {
+    if (isDigit.test(char)) {
+      collectedDigits.push(char);
+    }
+  }
+
+  return parseInt(collectedDigits.join(''), 10);
+};
+
+/**
+ * Checks that the meeting is within the specified range [workdayStart, workdayEnd]
+ *
+ * @param {string} workdayStart - Start time of the working day in the format hh:mm
+ * @param {string} workdayEnd - End time of the working day in the format hh:mm
+ * @param {string} meetingBegin - Start time of the meeting in the format hh:mm
+ * @param {number} meetingDuration - Meeting duration
+ * @returns {boolean}
+ */
+export const isMeetingOnTime = (workdayStart, workdayEnd, meetingBegin, meetingDuration) => {
+  const workdayStartMins = parseTime(workdayStart);
+  const meetingBeginMins = parseTime(meetingBegin);
+  if (workdayStartMins > meetingBeginMins) {
+    return false;
+  }
+
+  const workdayEndMins = parseTime(workdayEnd);
+  return meetingBeginMins + meetingDuration <= workdayEndMins;
+};
