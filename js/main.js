8ebf91b4e4b0829f66912b13cf6875c29625d20a
Ilya Cherepanov <cherepanov.ilya@gmail.com>
2026-04-09 00:13:09 +0700

    change async iife to top level await in main.js module

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ import { showDownloadErrorAlert } from './components/alert/download-alert.js';
 import { handleUploadImgInput } from './components/photo-form.js';
 import { fillDocumentWithPhotos } from './components/photos.js';
 
-(async () => {
-  const imgUploadElement = document.querySelector('.img-upload__input');
-  imgUploadElement.addEventListener('change', handleUploadImgInput);
+const imgUploadElement = document.querySelector('.img-upload__input');
+imgUploadElement.addEventListener('change', handleUploadImgInput);
 
-  /** @type {import('./api.js').Photo[] | null} */
-  let photos = null;
-  try {
-    photos = await getPhotos();
-  } catch {
-    showDownloadErrorAlert();
-    return;
-  }
+/** @type {import('./api.js').Photo[] | null} */
+let photos = null;
+try {
+  photos = await getPhotos();
+} catch {
+  showDownloadErrorAlert();
+}
+
+if (photos) {
   fillDocumentWithPhotos(photos);
-})();
+}
